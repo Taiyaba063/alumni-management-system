522d7aa9f5eded19afde5c7001011a4704b76daa
touhidulHoque <mhoque172106@bscse.uiu.ac.bd>
2023-10-27 22:18:42 +0600

    Initial project setup

---
+++ menu.php
@@ -0,0 +1,22 @@
+			<nav class="navbar">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li><a class="nav-in" href="index.php"><span data-letters="Home">Home</span></a> </li>
+						
+
+					</ul>
+				</div><!-- /.navbar-collapse -->
+
+			</nav>
